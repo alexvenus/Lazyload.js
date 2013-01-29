c3625f75f6186cd398aa93d5c795c9d65e5e1328
Benjamin Carl <opensource@clickalicious.de>
2013-01-29 10:15:28 +0100

    Update grunt.js
    fix

--- grunt.js
+++ grunt.js
@@ -43,7 +43,7 @@ module.exports = function (grunt) {
         },
         watch: {
             files: '<config:lint.files>',
-            tasks: 'lint'
+            tasks: 'default'
         },
         jshint: {
             options: {
